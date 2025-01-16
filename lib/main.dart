b8ecc66fe89a33b297b35617384b906224c67725
jason <originalkimchi100@gmail.com>
2025-01-16 13:13:15 +0900

    touch bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      builder: (context, child) => TouchIndicator(child: child!),
+      builder: (context, child) => TouchIndicator(child: child!, forceInReleaseMode: true,),
       home: TouchIndicator(
         // forceInReleaseMode: true,
         // enabled: true,
